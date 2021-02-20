1254bb9b4f6cd1e6f0cd6dbc4935ce935392fed3
Abbas Majidov <69363088+absmj@users.noreply.github.com>
2021-02-20 22:26:03 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+function AzLat2AzCyr(text, typ)
+{
+	// alert(1);
+	let 	regex 	=	null, letter = null, type = Math.abs(typ),
+			pattern	=	[
+						"A-А",
+						"a-а",
+						"B-Б",
+						"b-б",
+						"C-Ҹ",
+						"c-ҹ",
+						"Ç-Ч",
+						"ç-ч",
+						"D-Д",
+						"d-д",
+						"E-Е",
+						"e-е",
+						"Ə-Ә",
+						"ə-ә",
+						"F-Ф",
+						"f-ф",						
+						"G-Ҝ",
+						"g-ҝ",
+						"Ğ-Ғ",
+						"ğ-ғ",
+						"H-Һ",
+						"h-һ",						
+						"X-Х",
+						"x-х",
+						"I-Ы",
+						"ı-ы",
+						"İ-И",
+						"i-и",
+						"J-Ж",
+						"j-ж",
+						"K-К",
+						"k-к",
+						"Q-Г",
+						"q-г",						
+						"L-Л",
+						"l-л",
+						"M-М",
+						"m-м",
+						"N-Н",
+						"n-н",
+						"O-О",
+						"o-о",						
+						"Ö-Ө",
+						"ö-ө",
+						"P-П",
+						"p-п",
+						"R-Р",
+						"r-р",						
+						"S-С",
+						"s-с",
+						"Ş-Ш",
+						"ş-ш",
+						"T-Т",
+						"t-т",
+						"U-У",
+						"u-у",
+						"Ü-Ү",
+						"ü-ү",						
+						"V-В",
+						"v-в",
+						"Y-Ј",
+						"y-ј",
+						"Z-З",
+						"z-з"
+					];
+
+	for(let i in pattern)
+	{
+		letter = pattern[i].split("-");
+		regex = new RegExp(letter[0 + type], "gms");
+		text = text.replace(regex, letter[1 - type]);
+
+	}
+
+	return text;
+}
